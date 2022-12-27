90dce713e662acd29797bc1d6b7ed776a6bc5c10
Dominik Fuka <fukd00@vse.cz>
2022-12-27 15:40:05 +0100

    difficulty choice, alert when not enough questions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var selectedCategory;
+const amountQuestions = '10';
+
 $(document).ready(() => {
     // show categories as buttons when site loads
     const xhr = new XMLHttpRequest();
@@ -14,7 +17,7 @@ $(document).ready(() => {
                 });
             $('#categoryContainer').append(categoryNameButton);
             $('#' + item.id).addClass("btn btn-warning");
-            $('#' + item.id).click({ id: item.id }, CategoryButtonClicked);
+            $('#' + item.id).click({ id: item.id }, categoryButtonClicked);
         });
     });
     xhr.addEventListener('error', function (e) {
@@ -22,30 +25,43 @@ $(document).ready(() => {
     });
     xhr.send();
 
-    // assign functions to the nav bar buttons
-    $("#btnHomepage").click(ShowHomepage);
+    // skryti 
 });
 
-function CategoryButtonClicked(param) {
-    // load 20 questions from given category
+function categoryButtonClicked(param) {
+    // set selected category
+    selectedCategory = param.data.id;
+    // show difficulty selection
+    $('.categories').addClass('hidden');
+    $('.difficulty').removeClass('hidden');
+}
+
+function difficultyButtonClicked(difficulty) {
+    // load 10 questions from selected category of selected difficulty 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://opentdb.com/api.php?amount=20&category=' + param.data.id);
+    xhr.open('GET', 'https://opentdb.com/api.php?amount=' + amountQuestions + '&category=' + selectedCategory + '&difficulty=' + difficulty);
     xhr.addEventListener('load', () => {
         const data = JSON.parse(xhr.responseText);
-        console.log(data);
-        // hide categories and show questions
-        $('.categories').addClass("hidden");
-        $('.questions').removeClass("hidden");
-        // TODO complex showing of questions and checks, now only a list for now to try functionality
-        var questionNumber = 1;
-        $.each(data.results, function (index, item) {
-            var questionText = $('<p/>',
-                {
-                    text: questionNumber + ") " + item.question
-                });
-            $('#questionsContainer').append(questionText);
-            questionNumber++;
-        });
+        // TODO - questions only listed now
+        if (data.response_code == "0") {
+            // hide difficulty and show questions container
+            $('.difficulty').addClass("hidden");
+            $('.questions').removeClass("hidden");
+            // list fetched questions
+            listQuestions(data.results);
+        } else if (data.response_code == "1") {
+            // alert element
+            let errorMsg = $('<div id="alertNotEnoughQuestions" class="alert alert-danger"></div>').text('Sorry, not enough questions in selected category and difficulty yet. Please select another difficulty.');
+            // pop up alert above difficulty buttons
+            $('.difficulty > h1').after(errorMsg);
+            $("#alertNotEnoughQuestions").alert();
+            // slide up closing animation for alert after 5 sec
+            window.setTimeout(function () {
+                $("#alertNotEnoughQuestions").slideUp(500, function() {
+                    $(this).remove();
+                }); 
+            }, 6000);
+        }
     });
     xhr.addEventListener('error', function (e) {
         console.error('XHR error', e);
@@ -53,8 +69,26 @@ function CategoryButtonClicked(param) {
     xhr.send();
 }
 
-function ShowHomepage() {
-    $('.categories').removeClass("hidden");
-    $('.questions').addClass("hidden");
+const listQuestions = (questions) => {
+    console.log(questions);
+    $.each(questions, function (index, item) {
+        var questionText = $('<p/>',
+            {
+                text: index + 1 + ") " + decodeHTML(item.question)
+            });
+        $('#questionsContainer').append(questionText);
+    });
+}
+
+function showHomepage() {
+    $('.categories').removeClass('hidden');
+    $('.difficulty').addClass('hidden')
+    $('.questions').addClass('hidden');
     $('#questionsContainer').empty();
+}
+
+function decodeHTML(text) {
+    var textField = document.createElement("textarea");
+    textField.innerHTML = text;
+    return textField.value;
 }
